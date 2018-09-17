7c3419906df71d51bf595a78da885380254a3ffe
Pucoroyo <pucoroyo@gmail.com>
2018-09-17 14:11:23 +0700

    tahun kabisat generator

---
+++ tahun-kabisat.js
@@ -0,0 +1,8 @@
+for (let tahun = 1980; tahun <= 2018; tahun++) {
+    if (tahun % 4 === 0){
+    console.log(tahun + " -" + " INI TAHUN KABISAT")
+
+    }else {
+    console.log(tahun + "")
+    }
+}
